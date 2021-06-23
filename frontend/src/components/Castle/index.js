574a2a6dfc40a0f3f798710fc9e4f2756a0f511c
J0914 <jordynsechrist@gmail.com>
2021-06-23 12:57:50 -0500

    frontend updates to booking form

--- frontend/src/components/Castle/index.js
+++ frontend/src/components/Castle/index.js
@@ -1,18 +1,39 @@
-import React, { useEffect } from 'react';
+import React, { useState, useEffect } from 'react';
 import { useSelector, useDispatch } from 'react-redux';
 import { useParams } from 'react-router-dom';
 import * as spotsActions from '../../store/spots'
 
+
 import styles from '../../css-modules/Castle.module.css'
+
 
 export default function Castle () {
     const { id } = useParams();
     const dispatch = useDispatch();
     const castle = useSelector(state => state.spots.currentCastle)
+    const [end, setEnd] = useState('null');
+    
+    let today = new Date();
+    const dd = String(today.getDate()).padStart(2, '0');
+    const mm = String(today.getMonth() + 1).padStart(2, '0'); //January is 0!
+    const yyyy = today.getFullYear();
+    today = yyyy + '-' + mm + '-' + dd;
 
+    const [selectedStart, setSelectedStart] = useState(today);
+    const [selectedEnd, setSelectedEnd] = useState(selectedStart);
+    
     useEffect(() => {
         dispatch(spotsActions.getSingleSpot(id))
     }, [dispatch, id])
+
+    useEffect(() => {
+        setSelectedEnd(selectedStart)
+    }, [selectedStart])
+
+    const handleSubmit= () => {
+        return null;
+    }
+
 
     return (
         // <div className={styles.background}>
@@ -34,9 +55,43 @@ export default function Castle () {
                 <div id={styles.info}>
                     <p id={styles.description}>{castle.description}</p>
                 </div>
+                    <div id={styles.formDiv} >
+                    <form id={styles.bookingForm} onSubmit={handleSubmit}>
+                        <h3> Book a stay! </h3>
+                        {/* {errors.length ? 
+                        <div className={styles.errors}>
+                            <ul>
+                                {errors.map((error, idx) => <li key={idx}>{error}</li>)}
+                            </ul>
+                        </div> : null
+                        } */}
+                        <div className={styles.inputs}>
+                            <label>
+                                Start Date:
+                            </label>
+                            <input
+                            type="date"
+                            min={today}
+                            value={selectedStart}
+                            onChange={(e) => setSelectedStart(e.target.value)}
+                            required
+                            />
+                            <label>
+                                End Date:
+                            </label>
+                            <input
+                            type="date"
+                            value={selectedEnd}
+                            min={selectedStart}
+                            onChange={(e) => setSelectedEnd(e.target.value)}
+                            required
+                            />
+                            <button className={styles.submitBtn} type="submit">Book!</button>
+                        </div>
+                    </form>
+                    </div>
                 </>
                 }
             </div>
-        // </div>
     );
 }
